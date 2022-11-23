479ae879e43fe26fa3d59f3507535f192134d1cb
Ahmed Abu <ahmedabu13@gmail.com>
2022-11-22 18:45:36 -0600

    worked on addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,41 @@
 console.log('***** Music Collection *****')
+
+//- Add a function named `addToCollection`. This function should:
+//- Take in the album's `title`, `artist`, `yearPublished` as input parameters
+//- Create a new object having the above properties
+//- Add the new object to the end of the `collection` array
+//- Return the newly created object
+
+
+
+let collection=[] 
+
+songInfo= { 
+
+        title: " ", 
+        artist: " ", 
+        yearPublished: " "
+
+}
+
+    function addToCollection (title, artist, yearPublished ) { 
+        
+        addToCollection.push(songInfo)
+
+            return collection
+    }
+
+        
+
+
+    
+
+
+
+
+
+
+
+
+
+
